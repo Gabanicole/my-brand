02d0a3d5b35aaf43c2219d54aad20ccce5dcb8ff
Nicole <89022581+Gabanicole@users.noreply.github.com>
2023-01-23 15:17:08 +0200

    localstorage signup

--- assets/javascript/signup.js
+++ assets/javascript/signup.js
@@ -73,3 +73,44 @@ form.addEventListener('submit', function (e) {
   checkEmail(email);
   checkPasswordsMatch(password, password2);
 });
+
+
+//localstorage
+
+function signup(e){
+  event.preventDefault();
+
+
+var username = document.getElementById('username').value;
+var email = document.getElementById('email').value;
+var password = document.getElementById( 'password').value;
+
+
+var user ={
+  username: username,
+  email : email,
+  password: password,
+}
+var json = JSON.stringify(user);
+localStorage.setItem(username, json);
+console.log('user added')
+}
+
+function loginFun(e){
+  event.preventDefault();
+
+
+var username = document.getElementById('username').value;
+var email = document.getElementById('email').value;
+var password = document.getElementById( 'password').value;
+var result = document.getElementById('result').value;
+
+var user = localStorage.getItem(username);
+var data = JSON.parse(user);
+console.log(data);
+
+if (user = null){
+  result.innerHtml
+}
+
+}
